a0c76bb669bb441a8c453fbc5b9aaad145e16d5b
ashot1arzumanyan <ashot1arzumanyan@gmail.com>
2019-02-22 14:30:25 +0400

    fixed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import ReactDOM from "react-dom";
 import App from  './App'
 import style from './styles/index.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-module.hot.accept()
+ReactDOM.render(<App />, document.getElementById('root'))
